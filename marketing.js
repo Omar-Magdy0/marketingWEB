35b9552f51c656d1fb2621aed1159525d894958b
Omar-Magdy0 <omar.magdy.om.om@gmail.com>
2023-05-04 13:32:40 +0200

    marketingWEB

---
+++ marketing.js
@@ -0,0 +1,14 @@
+
+
+    var menuButton = document.getElementById("menuButton");
+    var menuButton = document.getElementById("menu");
+    
+    function toggle(){var displayed = false;
+       if(!displayed){ menu.style.display = "block";displayed = true}
+        else{menu.style.display = "none";displayed = none}
+    }  
+       menuButton.addEventListener('click',toggle);
+
+
+
+
