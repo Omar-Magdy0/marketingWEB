9fc4c19c7548c8099afe3c6e79176811d858fe25
Omar-Magdy0 <omar.magdy.om.om@gmail.com>
2023-05-04 14:01:23 +0200

    marketingWEB

--- marketing.js
+++ marketing.js
@@ -1,9 +1,18 @@
-
-console.log("12");
     var menuButton = document.getElementById("menuButton");
     var menu = document.getElementById("menu");
-    
-       menuButton.addEventListener('click',console.log("1"));
+    var displayed = false;
+ 
+    function toggle(){
+        if(!displayed){ menu.style.display = "block" ;menuButton.style.backgroundColor = "rgb(83, 83, 83)";
+        displayed = true;}
+         else{ menu.style.display = "none";menuButton.style.backgroundColor = "rgba(107, 107, 107, 0.89)"; displayed = false;}
+        
+        
+        
+        }  
+ 
+ 
+        menuButton.addEventListener('click',toggle);
 
 
 
